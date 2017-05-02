261302995afc2b1eb59f9b403eee99527c202540
Amir Rahwane <amir.rahwane@gmail.com>
2017-05-02 07:36:47 +0000

    commiting workspace for refernce

--- amirtest.js
+++ amirtest.js
@@ -16,46 +16,102 @@ const storage = require('@google-cloud/storage');
 
 
 
+    const bucket = 'koel';
+	const key = 'Mezdeke - Darbuka Show.mp3';
+
 	'use strict';
 
 	let tags = {};
 	let lyrics = ''; // Lyrics is handled differently
 
+	const projectId = process.env.GCP_PROJECTID;
 
+	const gcs = storage({
+	  projectId: projectId
+	});
 
-// Your Google Cloud Platform project ID
-const projectId = 'rahwane-farm';
+	const file = gcs.bucket(bucket).file(key);
 
-// Instantiates a client
-const gcs = storage({
-  projectId: projectId
-});
-
-
-  // References an existing bucket, e.g. "my-bucket"
-  // const bucket = gcs.bucket("koel");
-
-  // References an existing file, e.g. "file.txt"
-  // const file = bucket.file("muazzez ersoy ney kemence taksim.mp3");  
-  
-  
-  // References an existing bucket, e.g. "my-bucket"
-//  const bucket = bucket;
-
-  // References an existing file, e.g. "file.txt"
-  const file = gcs.bucket("koel").file("muazzez ersoy ney kemence taksim.mp3");
-
-  const options = {
+	const fileName = `/tmp/${Math.random().toString(36)}`;
+	const options = {
     // The path to which the file should be downloaded, e.g. "./file.txt"
-    destination: "./file.mp3"
-  };
+		destination: fileName
+	};
 
-  // Downloads the file
-  return file.download(options)
-    .then(() => {
-      console.log(`File ${file.name} downloaded to ./file.mp3`);
-    });
-	
-	
-	console.log(`Could not determine the media type.`);
+
+	async.waterfall([
+		function fetch(next) {
+
+			file.download(function(err, contents) { 
+				if (err) {
+					console.log(`Failed to fetch object from Google Cloud Storage: ${err}`);
+					return;
+					//return callback(`Failed to fetch object from Google Cloud Storage: ${err}`);
+				}
+				
+				fs.writeFileSync(fileName, contents);
+				console.log(`File ${file.name} downloaded to ${fileName}`);
+				const parser = mm(fs.createReadStream(fileName), {duration: true}, (err, rawTags) => {
+					if (err) {
+						console.error(`Error reading tags: ${err}.`);
+						return;
+					}
+
+					tags = rawTags;
+					next();
+				});
+
+				parser.on('ULT', result => {
+					lyrics = result.text;
+				});
+			});		
+		},
+
+		/**
+		 * Format the tags into something Koel can handle.
+		 * @param  {Function} next
+		 */
+		function formatTags(next) {
+			console.error(`Formatting Tags`);
+			tags.lyrics = lyrics;
+			tags.artist = tags.artist.length ? tags.artist[0] : '';
+			tags.albumartist = tags.albumartist.length ? tags.albumartist[0] : '';
+			tags.track = tags.track.no;
+
+			if (tags.picture.length) {
+				tags.cover = {
+					extension: tags.picture[0].format,
+					data: tags.picture[0].data.toString('base64')
+				};
+			}
+			delete tags.picture;
+
+			next();
+		},
+
+		function postToKoel() {
+			console.error(`Posting To Koel`);
+			request.post({
+				url: `${process.env.KOEL_HOST}/api/os/s3/song`,
+				form: {
+					bucket,
+					key,
+					tags,
+					appKey: process.env.KOEL_APP_KEY
+				}
+			}, err => {
+				if (err) {
+					console.log(`Error posting to Koel: ${err}.`);
+				}
+			});
+		}
+	], function (err) {
+		if (err) {
+			console.error(`Error: ${err}.`);
+		} else {
+			console.log(`Successfully sync ${key}`);
+		}
+
+		callback(null, 'Successful.');
+	});
 
